4287f36898ad3a67f29afc83496e09df21194d6b
anacarolcg0 <anacarolcg0@gmail.com>
2024-08-16 09:39:39 -0300

    Limpeza do boyler, componentização, modularização, Listagem. Inferência de Props.

---
+++ aula04/src/App.tsx
@@ -0,0 +1,28 @@
+import Cabecalho from "./components/Cabecalho/Cabecalho";
+import Conteudo from "./components/Conteudo/Conteudo";
+import Rodape from "./components/Rodape/Rodape";
+
+function App() {
+  return (
+    <div>
+      <Cabecalho />
+      <Conteudo />
+      <Rodape />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+// const App = () => {
+//   return (
+//     <div>
+//       <h1>Componente App</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+
