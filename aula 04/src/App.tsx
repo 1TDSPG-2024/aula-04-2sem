c9b3cae688e3b4adf20aa746159542be0762ef3e
Caepena <caepenafiel@gmail.com>
2024-08-16 09:10:51 -0300

    Criação da pasta components e importação dos mesmos.

---
+++ aula 04/src/App.tsx
@@ -0,0 +1,15 @@
+import Cabecalho from "./components/Cabecalho/Cabecalho";
+import Conteudo from "./components/Conteudo/Conteudo";
+import Rodape from "./components/Footer/Rodape";
+
+
+export default function App() {
+    return (
+        <div>
+            <Cabecalho />
+            <Conteudo />
+            <Rodape />
+        </div>
+    );
+}
+
---
+++ aula 04/src/components/Conteudo/Conteudo.tsx
@@ -0,0 +1,7 @@
+export default function Conteudo() {
+    return (
+        <main>
+            <h2></h2>
+        </main>
+    )
+}
