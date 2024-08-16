8b5feea32382c31df76ad8ac9a4773559252dbb9
Brunosz2712 <brunosouza_27@yahoo.com.br>
2024-08-16 09:36:34 -0300

    LImpeza boylerplate, componentização, modolarização, listagem, inferencia de props

---
+++ aula04/src/App.tsx
@@ -0,0 +1,35 @@
+import Cabecalho from "./components/Cabecalho/Cabecalho";
+import Conteudo from "./components/Conteudo/Conteudo";
+import Rodape from "./components/Rodape/Rodape";
+
+
+export default function App() {
+  return (
+    <div>
+      <Cabecalho />
+      <Conteudo />
+      <Rodape />
+    </div>
+  );
+}
+
+
+// function App() {
+//   return (
+//     <div>
+//       <h1>Compponente App</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+// const App = () => {
+//   return (
+//     <div>
+//       <h1> Componente App</h1>
+//     </div>
+//   )
+// }
