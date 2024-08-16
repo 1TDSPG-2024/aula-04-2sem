13dcaa5475571e70c8625355dab4a1365c8d45c2
victoraugustogfavaro <victoraugustogfavaro@gmail.com>
2024-08-16 09:46:58 -0300

    Props em lista com TYPES.

--- aula04/src/components/Conteudo/Conteudo.tsx
+++ aula04/src/components/Conteudo/Conteudo.tsx
@@ -2,15 +2,32 @@ import Lista from "../Lista/Lista";
 import ListaProps from "../Lista/ListaProps";
 
 export default function Conteudo(){
-    const nome = "Valor do nome";
+    const nomes = ["Francisco", "Juninho", "Roberto", "Ronaldinho"];
     return(
         <main>
             <h2>Conteúdo</h2>
             <Lista/>
             <div>
-                <p>Lista recebendo props</p>
-                <ListaProps nome={nome} />
+                <h2>Lista recebendo props</h2>
+                <ListaProps nomes={nomes} />
             </div>
         </main>
     )
 }
+
+// import Lista from "../Lista/Lista";
+// import ListaProps from "../Lista/ListaProps";
+
+// export default function Conteudo(){
+//     const nome = "Valor do nome";
+//     return(
+//         <main>
+//             <h2>Conteúdo</h2>
+//             <Lista/>
+//             <div>
+//                 <p>Lista recebendo props</p>
+//                 <ListaProps nome={nome} />
+//             </div>
+//         </main>
+//     )
+// }
--- aula04/src/components/Lista/ListaProps.tsx
+++ aula04/src/components/Lista/ListaProps.tsx
@@ -1,9 +1,25 @@
+// Passando um lista como props
 type ListaProps = {
-    nome: string;
+    nomes: string[];
 }
 
-export default function ListaProps({ nome }: ListaProps) {
+export default function ListaProps({ nomes }: ListaProps) {
     return (
-        <div>{nome}</div>
+        <ul>
+            {nomes.map((nome, i) => (
+                <li key={i}>{nome}</li>
+            ))}
+        </ul>
     )
 }
+
+// Props com string simples
+// type ListaProps = {
+//     nome: string;
+// }
+
+// export default function ListaProps({ nome }: ListaProps) {
+//     return (
+//         <div>{nome}</div>
+//     )
+// }
